aaf1310683c585805f2f856a8d932cecb6596b5d
wyy <40433185+330850927@users.noreply.github.com>
2019-08-19 22:31:55 +0800

    19.8.19在家学习1.30小时 学习PropTypes和defaultProps以及React调试工具 React-Developer Tools

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,18 +1,31 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
 class TodoItem extends Component{
     constructor(props){
         super(props);
         this.handleClick=this.handleClick.bind(this);
     }
     render(){
+        const {content,test}=this.props;
         return(
             <div onClick={this.handleClick}>
-                {this.props.content}
+                {test}-{content}
             </div>
         )
     }
     handleClick(){
         this.props.deleteItem(this.props.index)
     }
+}
+TodoItem.propTypes={
+    test:PropTypes.string.isRequired,
+    content:PropTypes.string.isRequired,
+    deleteItem:PropTypes.func.isRequired,
+    index:PropTypes.number.isRequired
+
+}
+TodoItem.defaultProps={
+    test:'hello world'
 }
 export default TodoItem;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import TodoList from './TodoList';
-import TodoItem from './TodoItem';
+
 ReactDOM.render(<TodoList />, document.getElementById('root'));
-ReactDOM.render(<TodoItem />, document.getElementById('item'));
+
 
 
 
